ed6a508f3d411795a4800dd9f16b9311831e66db
cheungbo <cheungbo@users.noreply.github.com>
2026-05-11 08:11:30 +0800

    clean: 移除调试脚本和备份文件

--- check_docs.js
+++
@@ -1,4 +0,0 @@
-const Database = require("better-sqlite3");
-const db = new Database("./data/weeklydata.db");
-const docs = db.prepare("SELECT id, substr(updated_at,1,16) as updated, length(data) as size FROM documents ORDER BY id").all();
-console.log(JSON.stringify(docs, null, 2));
